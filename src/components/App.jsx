d3027e936767628a49028891b76d6efb67fd8ce8
DmitriyCherkaska <151064884+DmitriyCherkaska@users.noreply.github.com>
2024-05-25 23:24:37 +0300

    dsffsdf

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,23 @@
 // import Options from "./Options/Options";
 // import Feedback from "./Feedback/Feedback";
 // import Description from "./Description/Description";
-import ContactForm from "./ContactForm/ContactForm";
+import ContactForm from '../components/ContactForm/ContactForm';
 // import { useEffect } from "react";
 // import { useState } from "react";
 import "./App.css";
 
-const App = () => {
+function App() {
+
+  const submit = (data) => {
+    console.log('data :>> ', data);
+  }
 
   return (
-    <div>
-  <h1>Phonebook</h1>
-  <ContactForm />
-  <SearchBox />
-  <ContactList />
-</div>
-  );
-};
+    <>
+   <ContactForm submit={submit} />; 
+   </>
+  )
+   
+}
 
 export default App;
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,15 +1,51 @@
-import some from './ContactForm'
+// import some from './ContactForm.module.css'
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from "yup";
+import { useId } from "react";
+// import {nanoid} from 'nanoid';
 
-const Description = ({ title, text }) => {
-  return (
-    <div>
-      <h1 className={some.title}>Sip Happens Café: {title}</h1>
-      <p className={some.text}>
-        Please leave your feedback about our service by selecting one of the
-        options below.: {text}
-      </p>
-    </div>
-  );
+const ContactFormSchema = Yup.object().shape({
+  username: Yup.string().min(2,'Too Short!').max(50, 'Too Long!'). required('Required'),
+  number: Yup.string().matches(/^\d{3}-\d{2}-\d{2}$/,'Number must be in format 123-45-67').required('Required'),
+});
+
+  const initialValues = {
+    username: "Name",
+    number:'Number'
+  };
+
+const ContactForm = ({ submit }) => {
+  const nameFieldId = useId();
+  const numberFieldId = useId();
+
+  const handleSubmit = (values, actions) => {
+    console.log(values);
+    actions.resetForm();
+  };
+
+return (
+  <Formik 
+    initialValues={initialValues} 
+    onSubmit={handleSubmit} 
+    validateYupSchema={ContactFormSchema}
+    >
+    <Form>
+      <div>
+      <label htmlFor="nameFieldId">Name</label>
+      <Field type='text' name='username' id={nameFieldId} />
+      <ErrorMessage name='username' component='span' />
+      </div>
+
+      <div>
+      <label htmlFor="numberFieldId">Number</label>
+      <Field type='number' name='phone' id={numberFieldId} />
+      <ErrorMessage name='phone' component='span' />
+      </div>
+
+      <button type='submit'>Add contact</button>
+    </Form>
+  </Formik>
+);
 };
 
 export default ContactForm;
