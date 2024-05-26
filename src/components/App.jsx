f2adb375728c1e8cc790592489f4772bc302183e
DmitriyCherkaska <151064884+DmitriyCherkaska@users.noreply.github.com>
2024-05-26 21:48:03 +0300

    sds

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,35 +1,43 @@
-import { useState, useEffect } from 'react';
-import ContactForm from './ContactForm/ContactForm';
-import SearchBox from './SearchBox/SearchBox';
-import ContactList from './ContactList/ContactList';
+import { useState, useEffect } from "react";
+import ContactForm from "./ContactForm/ContactForm";
+import SearchBox from "./SearchBox/SearchBox";
+import ContactList from "./ContactList/ContactList";
 
 const App = () => {
-  const [contacts, setContacts] = useState(() => {
-    const savedContacts = localStorage.getItem('contacts');
-    if (savedContacts) {
-      return JSON.parse(savedContacts);
-    } else {
-      return [];
-    }
-  });
+  const initialContacts = [
+    { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+    { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+    { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+    { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+  ];
 
-  const [filter, setFilter] = useState('');
-  
-  const filteredContacts = contacts.filter(contact => contact.name.toLowerCase().includes(filter.toLowerCase()));
+  const [contacts, setContacts] = useState(initialContacts);
+  const [filter, setFilter] = useState("");
+
+  useEffect(() => {
+    const storedContacts = localStorage.getItem("contacts");
+    if (storedContacts) {
+      setContacts(JSON.parse(storedContacts));
+    }
+  }, []);
 
   const addContact = (newContact) => {
     setContacts([...contacts, newContact]);
   };
 
   const deleteContact = (id) => {
-    const updatedContacts = contacts.filter(contact => contact.id !== id);
+    const updatedContacts = contacts.filter((contact) => contact.id !== id);
     setContacts(updatedContacts);
   };
 
   useEffect(() => {
-    localStorage.setItem('contacts', JSON.stringify(contacts));
+    localStorage.setItem("contacts", JSON.stringify(contacts));
   }, [contacts]);
 
+  const filteredContacts = contacts.filter((contact) =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
+  
   return (
     <div>
       <h1>Phonebook</h1>
