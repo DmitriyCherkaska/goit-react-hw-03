22ef5e55a1ef483d9c54f9c7190f57bd2380ca4c
DmitriyCherkaska <151064884+DmitriyCherkaska@users.noreply.github.com>
2024-05-27 23:04:49 +0300

    фывфыв

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,17 +1,16 @@
 // import some from './ContactForm.module.css'
 // import { useId } from "react";
-import { ErrorMessage, useFormik } from "formik";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 import {nanoid} from 'nanoid';
 
 const ContactForm = ({ addContact }) => {
-  const formik = useFormik({
-   initialValues : {
+  const initialValues = {
     name: '',
     number: '',
-   },
+  };
 
-  ContactFormSchema: Yup.object().shape({
+  const ContactFormSchema = Yup.object().shape({
     name: Yup.string()
       .min(2, "Too Short!")
       .max(50, "Too Long!")
@@ -19,34 +18,42 @@ const ContactForm = ({ addContact }) => {
     number: Yup.string()
       .matches(/^\d{3}-\d{2}-\d{2}$/, "Number must be in format 123-45-67")
       .required("Required"),
-  }),
+  });
 
-  handleSubmit: (values, { resetForm }) => {
-    addContact({
-        id: nanoid(),
-        name: values.name,
-        number: values.number,
-      });
-      resetForm();
-    },
-});
+  const handleSubmit = (values, { resetForm }) => {
+    console.log(event);
+    const newContact = {
+      id: nanoid(),
+      name: values.name,
+      number: values.number,
+    };
+    console.log(newContact);
+    addContact(newContact);
+    resetForm();
+  }
 
   return (
-    <form onSubmit={formik.handleSubmit}>
+    <Formik
+      initialValues={initialValues}
+      onSubmit={handleSubmit}
+      validateYupSchema={ContactFormSchema}
+    >
+      <Form>
         <div>
           <label htmlFor='name'>Name</label>
-          <input type="text" name="name" id='name' />
+          <Field type="text" name="name" id='name' />
           <ErrorMessage name="name" component="span" />
         </div>
 
         <div>
           <label htmlFor='number'>Number</label>
-          <input type="text" name="number" id='number' />
+          <Field type="text" name="number" id='number' />
           <ErrorMessage name="number" component="span" />
         </div>
 
         <button type="submit">Add contact</button>
-    </form>
+      </Form>
+    </Formik>
   );
 };
 
