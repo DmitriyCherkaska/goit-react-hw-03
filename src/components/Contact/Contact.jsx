35784418a696abccba4f0d9079087bf7d3f030c1
DmitriyCherkaska <151064884+DmitriyCherkaska@users.noreply.github.com>
2024-05-29 16:21:25 +0300

    fix style css

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,10 +1,24 @@
+import some from './Contact.module.css'
 
 const Contact = ({ contact, deleteContact }) => {
   return(
-  <li>
-    {contact.name} - {contact.number}
-    <button onClick={() => deleteContact(contact.id)}>Delete</button>
+  <li className={some.item}>
+    <div className={some.container}>
+
+    <div className={some.contacts}>
+    <span className={some.username}>{contact.name} 
+    </span>
+    <span className={some.number}>{contact.number} 
+    </span>
+    </div>
+
+    <div className={some.wrapper}>
+     <button className={some.btn} onClick={() => deleteContact(contact.id)}>Delete
+     </button> 
+    </div>
+  </div>
   </li>
+
   );
 }
 
